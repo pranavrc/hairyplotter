372f23628c79cc950a726979f89e38fb459c51db
Pranav Ravichandran <me@onloop.net>
2013-02-18 22:44:21 +0530

    processing.py import

---
+++ processing.py-0021/examples.py/Python/colornames/colorsketch.py
@@ -0,0 +1,19 @@
+"""
+Demonstrates using a third-party Python library
+author: Jonathan Feinberg
+"""
+from namethatcolor import NameThatColor
+
+flag = loadImage("flag.jpg")
+namer = NameThatColor()
+
+def setup():
+    size(200, 200)
+
+def draw():
+    background(0)
+    image(flag, 75, 40 )
+    if mouseX >= 0 and mouseX <= width and mouseY >= 0 and mouseY <= height:
+        c = 0x00FFFFFF & get(mouseX, mouseY)
+        fill(c >> 16, c >> 8 & 0xFF, c & 0xFF)
+        text(namer.name(hex(c)[2:]).name, 75, 120)
