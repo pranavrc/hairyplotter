1bd5fa61ce716a1e29a314bc891daaf4b7c34199
Pranav Ravichandran <me@onloop.net>
2013-03-01 22:32:22 +0530

    Moved dataset load out of loop.

--- plug.py
+++ plug.py
@@ -12,13 +12,14 @@ port = str(raw_input("Serial Port: "))
 baudrate = str(raw_input("Baud Rate: "))
 ser = serial.Serial(port, baudrate)
 
+ref = cPickle.load(open('dataset.p', 'rb'))
+ref = ref.values()
+ref = zip(ref, [0] * len(ref))
+
 while True:
     while count in range(a.maxVal):
         readingList.append(int(ser.readline().strip('\x00\r\n')))
         readingList = zip(readingList, [0] * len(readingList))
-        ref = cPickle.load(open('dataset.p', 'rb'))
-        ref = ref.values()
-        ref = zip(ref, [0] * len(ref))
         scaled = factory.scale([factory.retSimilarity(ref, readingList, 1), \
                                 factory.retSimilarity(ref, readingList, 2), \
                                 factory.retSimilarity(ref, readingList, 3)])
