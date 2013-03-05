0cf13947e2f82565f9c90c0bac42df2883e9f8c7
Pranav Ravichandran <me@onloop.net>
2013-03-05 15:22:46 +0530

    Basic serial printing.

---
+++ eyeda.ino
@@ -0,0 +1,26 @@
+const int analogInPin1 = A0;
+const int analogInPin2 = A1;
+int sensorValue1 = 0;
+int sensorValue2 = 0;
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600); 
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue1 = analogRead(analogInPin1);
+  sensorValue2 = analogRead(analogInPin2);
+  
+  // print the results to the serial monitor:
+  Serial.print(sensorValue1);
+  Serial.print(",");
+  Serial.println(sensorValue2);
+  
+  // wait 2 milliseconds before the next loop
+  // for the analog-to-digital converter to settle
+  // after the last reading:
+  delay(2);                     
+}
+
+
