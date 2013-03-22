5ed2c018c1240b95807aa2a8099531e6c0f1fce7
Pranav Ravichandran <me@onloop.net>
2013-03-22 09:10:22 +0530

    Server sent events for browser communication with eye movements.

--- plug.py
+++ plug.py
@@ -4,6 +4,12 @@ import factory
 import calib
 import serial
 import cPickle
+import gevent
+import gevent.monkey
+from flask import Flask, request, Response, render_template
+
+gevent.monkey.patch_all()
+app = Flask(__name__)
 
 a = calib.Calibrator()
 readingList = []
@@ -16,22 +22,35 @@ ref = cPickle.load(open('dataset.p', 'rb'))
 ref = ref.values()
 #ref = zip(ref, [0] * len(ref))
 
-while True:
-    readingList = []
-    count = 0
+def stream_data():
+    while True:
+        readingList = []
+        count = 0
 
-    while (count < a.maxVal):
-        try:
-            (l, r) = ser.readline().strip('\x00\r\n').strip().split(',')
-            readingList.append((int(l), int(r)))
-        except:
-            continue
+        while (count < a.maxVal):
+            try:
+                (l, r) = ser.readline().strip('\x00\r\n').strip().split(',')
+                readingList.append((int(l), int(r)))
+            except:
+                continue
 
-        count += 1
-        #readingList = zip(readingList, [0] * len(readingList))
+            count += 1
+            #readingList = zip(readingList, [0] * len(readingList))
 
-    scaled = factory.scale([factory.retSimilarity(readingList, ref, 1), \
-                            factory.retSimilarity(readingList, ref, 2), \
-                            factory.retSimilarity(readingList, ref, 3)])
+        scaled = factory.scale([factory.retSimilarity(readingList, ref, 1), \
+                                factory.retSimilarity(readingList, ref, 2), \
+                                factory.retSimilarity(readingList, ref, 3)])
 
-    print factory.classify(scaled)
+        yield 'data: %s\n\n' %  factory.classify(scaled)
+
+@app.route('/stream')
+def stream():
+    return Response(stream_data(),
+                    mimetype = 'text/event-stream')
+
+@app.route('/')
+def index():
+    return render_template('client.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
