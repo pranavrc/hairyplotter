1d6dda939b0a84ad970c0705c7663a059b16f688
Pranav Ravichandran <me@onloop.net>
2013-12-11 23:16:46 +0530

    Better naming

--- readandprint.ino
+++ readandprint.ino
@@ -1,24 +1,29 @@
-const int analogInPin1 = A0;
-const int analogInPin2 = A1;
-int sensorValue1 = 0;
-int sensorValue2 = 0;
+// LE - Left Electrode, RE - Right Electrode.
+
+const int analogInPin_LE = A0;
+const int analogInPin_RE = A1;
+
+int sensorValue_LE = 0;
+int sensorValue_RE = 0;
+
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600); 
+  // Initialize serial communication at 9600 bps.
+  Serial.begin(9600);
 }
 
 void loop() {
-  // read the analog in value:
-  sensorValue1 = analogRead(analogInPin1);
-  sensorValue2 = analogRead(analogInPin2);
-  
-  // print the results to the serial monitor:
-  Serial.print(sensorValue1);
+  // Read the analog input value.
+  sensorValue_LE = analogRead(analogInPin_LE);
+  sensorValue_RE = analogRead(analogInPin_RE);
+
+  // Print the results to the serial monitor,
+  // delimiting the two values with a comma.
+  Serial.print(sensorValue_LE);
   Serial.print(",");
-  Serial.println(sensorValue2);
-  
-  // wait 2 milliseconds before the next loop
+  Serial.println(sensorValue_RE);
+
+  // Wait for 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
-  // after the last reading:
-  delay(2);                     
+  // after the last reading.
+  delay(2);
 }
