6a1814ad7e722d66cabd272b3991f9a597660a2f
Pranav Ravichandran <me@onloop.net>
2013-12-12 10:05:04 +0530

    Docs

--- cursor.py
+++ cursor.py
@@ -7,23 +7,37 @@ import calib
 import serial
 import cPickle
 
+# Create a Calibrator object.
 a = calib.Calibrator()
+
+# Open up the serial port.
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
+# Deserialize the stored dataset from calibration
+# Extract the lists of tuples of values from the dict.
 ref = cPickle.load(open('datasets.p', 'rb'))
 ref = ref.values()
 
 mouse = PyMouse()
+
+# Screen resolution length and height.
 resL, resH = mouse.screen_size()[0], mouse.screen_size()[1]
 lengthCenter, heightCenter = resL/2, resH/2
+
+# Pixels to move by. Vertical is 1 and corresponding
+# horizontal for diagonal movement is calculated.
 verticalMove = 1
-horizontalMove = (resL/resH) * verticalMove
+horizontalMove = (resL/float(resH)) * verticalMove
+
+# Place the cursor at the center of the screen initially.
 mouse.move(lengthCenter, heightCenter)
 
+# Watch for eye movements, classify and move cursor.
 while True:
     readingList = []
     count = 0
 
+    # maxVal is the size of the dataset as specified by the user.
     while (count < a.maxVal):
         try:
             (l, r, _) = ser.readline().strip('\x00\r\n').strip().split(',')
@@ -33,16 +47,19 @@ while True:
 
         count += 1
 
+    # Calculate similarity, scale and classify.
     scaled = factory.scale([factory.retSimilarity(readingList, ref, 1), \
                             factory.retSimilarity(readingList, ref, 2), \
-                            factory.retSimilarity(readingList, ref, 3)])
-
+                            factory.retSimilarity(readingList, ref, 3),
+                            factory.retSimilarity(readingList, ref, 4)])
     direction = factory.classify(scaled)
     print direction
-    
+
     l, h = mouse.position()[0], mouse.position()[1]
     mouse.move(l, h)
-    
+
+    # Decrease or increase the vertical/horizontal position based on the
+    # direction of the eyes.
     if direction == "STRAIGHT":
         pass
     elif direction == "UP":
@@ -85,4 +102,5 @@ while True:
         l, h = mouse.position()[0], mouse.position[1]
         mouse.click(l, h, 1)
 
+    # Clear input buffer before iterating.
     ser.flushInput()
