a6dce71e1f2d0190f252b0911bfd2caebf5c78c8
Pranav Ravichandran <me@onloop.net>
2014-04-22 20:33:21 +0530

    LDA Prediction function

--- svm.py
+++ svm.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from sklearn import svm
+from sklearn import svm, lda
 import numpy as np
 
-class SVM_Classify:
+class Classify:
     def __init__(self, labelled):
+        ''' Set up the dataset and target as numpy arrays. '''
         self.positions = {1 : 'BLINK',
                           2 : 'UP',
                           3 : 'UP-RIGHT',
@@ -19,12 +20,27 @@ class SVM_Classify:
         self.dataset = np.array(labelled)
         self.dataset = self.dataset.reshape((len(self.dataset), -1))
         self.targets = np.array(self.positions.keys())
-        self.targets=[1,2]
 
+        self.clf = None
+
+    def svm_predict(self, unlabelled):
+        ''' Use a Support Vector Machine for classification. '''
         self.clf = svm.SVC()
         self.clf.fit(self.dataset, self.targets)
 
-    def predict(self, unlabelled):
+        unlabelled = np.array(unlabelled)
+        unlabelled = unlabelled.flatten()
+
+        target = self.clf.predict(unlabelled)
+        return self.positions[target[0]]
+
+    def lda_predict(self, unlabelled):
+        ''' Use Linear Discriminant Analysis for classification.
+        WARNING: Will only work when we have multiple data samples
+        for each dataset (i.e., two for left, two for right, etc.)'''
+        self.clf = lda.LDA()
+        self.clf.fit(self.dataset, self.targets)
+
         unlabelled = np.array(unlabelled)
         unlabelled = unlabelled.flatten()
 
